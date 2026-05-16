5de43287ff10be3feda63a145772f33b5fe1ea2f
bdawg117 <brimandev@gmail.com>
2026-05-16 19:50:20 -0400

    now piping gz sim to webpage!

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,14 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-950 flex items-center justify-center">
       <div className="flex transition-all duration-500"></div>
-       <div className={selectPanel ? "w-150" : "w-auto"}></div>
+      <div className={selectPanel ? "w-150" : "w-auto"}></div>
 
       <Terminal
         panelSelect={setSelectPanel}
         activePanel={selectPanel !== null}
-        // onOpen={() => setShowResume(true)}
-        // onClose={() => setShowResume(false)}
-        // isOpen={showResume}
+      // onOpen={() => setShowResume(true)}
+      // onClose={() => setShowResume(false)}
+      // isOpen={showResume}
 
       // onResume={() => setShowResume(prev => !prev)}
       />
@@ -27,18 +27,22 @@ function App() {
         </pre>
       )} */}
 
-      { selectPanel === 'resume' && (
+      {selectPanel === 'resume' && (
         <pre className="w-2/3 h-[80vh] bg-gray-800 text-green-300 font-mono p-6 rounded-lg ml-4 overflow-auto">
           {resumeText}
         </pre>
       )}
 
-      { selectPanel === 'ros2' && (
-        <div className='w-2/3 h-[80vh] p-6 rounded-lg ml-4 text-green-800'>
-          <div >ROS2!</div>
-          <div>CONTROLS HERE</div>
+      {selectPanel === 'ros2' && (
+        <div className="w-2/3 h-[80vh] bg-gray-800 rounded-lg ml-4 flex flex-col">
+          <iframe
+            src="http://192.168.1.69:6080/vnc.html?host=192.168.1.69&port=6080&autoconnect=true&resize=scale"
+            className="w-full flex-1 rounded-t-lg"
+          />
+          <div className="text-green-300 font-mono p-4 border-t border-gray-600">
+            CONTROLS HERE
+          </div>
         </div>
-
       )}
 
     </div>
